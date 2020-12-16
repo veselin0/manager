2f7b20e05cea4634a2b9f05731bc41cd5c53a158
Veselin <vesselin60@gmail.com>
2020-12-16 17:11:25 +0200

    372. Handling Form Updates with Action Creators

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import { Card, CardSection, Button, Input } from './common';
 
 class LoginForm extends Component {
+  onEmailChange(text) {
+
+  }
+
   render() {
     return (
       <Card>
         <CardSection>
-          <Input label="Email" placeholder="email@email.com" />
+          <Input
+            label="Email"
+            placeholder="email@email.com"
+            onChangeText={this.onEmailChange.bind(this)}
+          />
         </CardSection>
 
         <CardSection>
