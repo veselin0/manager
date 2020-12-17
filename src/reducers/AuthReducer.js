79cef5b373519c5856b21d6ee18b4e0ccc4c483f
Veselin <vesselin60@gmail.com>
2020-12-17 14:08:43 +0200

    375. Immutable State

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -5,7 +5,7 @@ const INITIAL_STATE = { email: '' };
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMAIL_CHANGED:
-      
+
     default:
       return state;
   }
