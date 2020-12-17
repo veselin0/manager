eb91c81edcd019a6575109a369bf9b85af608b3f
Veselin <vesselin60@gmail.com>
2020-12-17 17:22:22 +0200

    380. Redux Thunk in Practice

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,8 +16,10 @@ export const passwordChanged = (text) => {
 };
 
 export const loginUser = ({ email, password }) => {
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then((user) => console.log(user));
+  return (dispatch) => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((user) => console.log(user));
+  };
 };
