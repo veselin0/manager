68ea2cfe759b7f0b73e23a8427d510cc65667c33
Veselin <vesselin60@gmail.com>
2020-12-22 17:39:22 +0200

    398. Dynamic Property Updates

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
+import { toCppNamespace } from 'react-native/ReactCommon/hermes/inspector/tools/msggen/src/Converters';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
-export default class EmployeeCreate extends Component {
+class EmployeeCreate extends Component {
   render() {
     return (
       <Card>
         <CardSection>
-          <Input label="Name" placeholder="Jane" />
+          <Input
+            label="Name"
+            placeholder="Jane"
+            value={this.props.name}
+            onChangeText={(value) =>
+              this.props.employeeUpdate({
+                prop: 'name',
+                value,
+              })
+            }
+          />
         </CardSection>
 
         <CardSection>
-          <Input label="Phone" placeholder="555-555´-5555" />
+          <Input
+            label="Phone"
+            placeholder="555-555´-5555"
+            value={this.props.phone}
+            onChangeText={(value) =>
+              this.props.employeeUpdate({
+                prop: 'phone',
+                value,
+              })
+            }
+          />
         </CardSection>
 
         <CardSection />
@@ -22,3 +45,11 @@ export default class EmployeeCreate extends Component {
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm;
+
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
