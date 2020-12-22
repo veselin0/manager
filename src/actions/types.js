f6e60532acf47dae9b31a2660e39d75ac184b151
Veselin <vesselin60@gmail.com>
2020-12-22 15:50:51 +0200

    396. Employee Form Actions

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,4 +3,4 @@ export const PASSWORD_CHANGED = 'password_changed';
 export const LOGIN_USER_SUCCESS = 'login_user_success';
 export const LOGIN_USER_FAIL = 'login_user-fail';
 export const LOGIN_USER = 'login_user';
-
+export const EMPLOYEE_UPDATE = 'employee_update';
