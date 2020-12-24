05718d88a77b756b58502981f50cd887c1ea9d26
Veselin <vesselin60@gmail.com>
2020-12-24 10:18:01 +0200

    403. Creating Records with Firebase

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import { Text, StyleSheet } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeCreate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
+  onButtonPress() {
+    const { name, phone, shift } = this.props;
+
+    this.props.employeeCreate({ name, phone, shift });
+  }
+
   render() {
     return (
       <Card>
@@ -56,7 +62,7 @@ class EmployeeCreate extends Component {
         </CardSection>
 
         <CardSection>
-          <Button>Create</Button>
+          <Button onPress={this.onButtonPress.bind(this)}>Create</Button>
         </CardSection>
       </Card>
     );
@@ -83,4 +89,6 @@ const styles = StyleSheet.create({
   },
 });
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
+export default connect(mapStateToProps, { employeeUpdate, employeeCreate })(
+  EmployeeCreate,
+);
