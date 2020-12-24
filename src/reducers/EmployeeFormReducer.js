d434a3ecaecc6a66585aebb4c2fb5c6b22b0a6ca
Veselin <vesselin60@gmail.com>
2020-12-24 10:27:33 +0200

    404. Default Form Values

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -3,7 +3,7 @@ import { EMPLOYEE_UPDATE } from '../actions/types';
 const INITIAL_STATE = {
   name: '',
   phone: '',
-  shift: '',
+  shift: 'Monday',
 };
 
 export default (state = INITIAL_STATE, action) => {
