376c81b8a0038aaf31f651369ea9ea767760ae65
Veselin <vesselin60@gmail.com>
2020-12-29 15:58:28 +0200

    414. Reusable Forms

---
+++ src/components/EmployeeForm.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
+import { CardSection, Input } from './common';
+
+class EmployeeForm extends Component {
+  render() {
+    return (
+      <View>
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="Jane"
+            value={this.props.name}
+            onChangeText={(value) =>
+              this.props.employeeUpdate({
+                prop: 'name',
+                value,
+              })
+            }
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Phone"
+            placeholder="555-555´-5555"
+            value={this.props.phone}
+            onChangeText={(value) =>
+              this.props.employeeUpdate({
+                prop: 'phone',
+                value,
+              })
+            }
+          />
+        </CardSection>
+
+        <CardSection style={styles.cardSectionStyle}>
+          <Text style={styles.pickerTextStyle}>Shift</Text>
+          <Picker
+            style={styles.pickerStyle}
+            selectedValue={this.props.shift}
+            onValueChange={(value) =>
+              this.props.employeeUpdate({ prop: 'shift', value })
+            }>
+            <Picker.Item label="Monday" value="Monday" />
+            <Picker.Item label="Tuesday" value="Tuesday" />
+            <Picker.Item label="Wednesday" value="Wednesday" />
+            <Picker.Item label="Thursday" value="Thursday" />
+            <Picker.Item label="Friday" value="Friday" />
+            <Picker.Item label="Saturday" value="Saturday" />
+            <Picker.Item label="Sunday" value="Sunday" />
+          </Picker>
+        </CardSection>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  pickerTextStyle: {
+    fontSize: 18,
+    paddingLeft: 20,
+  },
+  pickerStyle: {
+    flex: 1,
+    paddingTop: 100,
+  },
+  cardSectionStyle: {
+    flexDirection: 'column',
+  },
+});
+
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm;
+
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeForm);
