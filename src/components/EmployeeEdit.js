49251e9a969d8943ea3c13f72d4d4c97e815cb78
Veselin <vesselin60@gmail.com>
2020-12-29 16:34:49 +0200

    416. Initializing Forms from State

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,19 +1,38 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import EmployeeForm from './EmployeeForm';
+import { employeeUpdate } from '../actions';
 import { Card, CardSection, Button } from './common';
 
 class EmployeeEdit extends Component {
+  componentDidMount() {
+    _.each(this.props.employee, (value, prop) => {
+      this.props.employeeUpdate({ prop, value });
+    });
+  }
+
+  onButtonPress() {
+    const { name, phone, shift } = this.props;
+    console.log(name, phone, shift);
+  }
+
   render() {
     return (
       <Card>
         <EmployeeForm />
         <CardSection>
-          <Button>Save Changes</Button>
+          <Button onPress={this.onButtonPress.bind(this)}>Save Changes</Button>
         </CardSection>
       </Card>
     );
   }
 }
 
-export default connect()(EmployeeEdit);
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm;
+
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeEdit);
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -5,7 +5,7 @@ import { CardSection } from './common';
 
 class ListItem extends Component {
   onRowPress() {
-    Actions.employeeCreate({ employee: this.prop.employee });
+    Actions.employeeEdit({ employee: this.props.employee });
   }
 
   render() {
