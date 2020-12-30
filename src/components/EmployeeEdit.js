eba12a7dcdbaebcecd71dc994537be19f1afff24
Veselin <vesselin60@gmail.com>
2020-12-30 09:49:44 +0200

    424. Wiring up Employee Delete

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
-import { employeeUpdate, employeeSave } from '../actions';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
 import { Card, CardSection, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
@@ -33,7 +33,9 @@ class EmployeeEdit extends Component {
   }
 
   onAccept() {
+    const { uid } = this.props.employee;
 
+    this.props.employeeDelete({ uid });
   }
 
   onDecline() {
@@ -77,6 +79,8 @@ const mapStateToProps = (state) => {
   return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate, employeeSave })(
-  EmployeeEdit,
-);
+export default connect(mapStateToProps, {
+  employeeUpdate,
+  employeeSave,
+  employeeDelete,
+})(EmployeeEdit);
