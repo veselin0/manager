0db3c489a7471c59b8b817b3574c5909f9758e69
Veselin <vesselin60@gmail.com>
2020-12-30 09:13:16 +0200

    422. Styling the Modal

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -4,9 +4,11 @@ import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
 import { employeeUpdate, employeeSave } from '../actions';
-import { Card, CardSection, Button } from './common';
+import { Card, CardSection, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
+  state = { showModal: false };
+
   componentDidMount() {
     _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value });
@@ -42,6 +44,17 @@ class EmployeeEdit extends Component {
         <CardSection>
           <Button onPress={this.onTextPress.bind(this)}>Text Schedule</Button>
         </CardSection>
+
+        <CardSection>
+          <Button
+            onPress={() => this.setState({ showModal: !this.state.showModal })}>
+            Fire Employee
+          </Button>
+        </CardSection>
+
+        <Confirm visible={this.state.showModal}>
+          Are you sure you want to delete this?
+        </Confirm>
       </Card>
     );
   }
--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import { Text, View, Modal} from 'react-native';
+import { Text, View, Modal, StyleSheet } from 'react-native';
 import { CardSection } from './CardSection';
 import { Button } from './Button';
 
 const Confirm = ({ children, visible, onAccept, onDecline }) => {
+  const { containerStyle, textStyle, cardSectionStyle } = styles;
+
   return (
     <Modal
       visible={visible}
       transparent
       animationType="slide"
-      onRequestClose={() => {}}
-    >
-      <CardSection>
-        <Text>{children}</Text>
-      </CardSection>
+      onRequestClose={() => {}}>
+      <View style={containerStyle}>
+        <CardSection style={cardSectionStyle}>
+          <Text style={textStyle}>{children}</Text>
+        </CardSection>
 
-      <CardSection>
-        <Button onPress={onAccept}>Yes</Button>
-        <Button onPress={onDecline}>No</Button>
-      </CardSection>
+        <CardSection style={cardSectionStyle}>
+          <Button onPress={onAccept}>Yes</Button>
+          <Button onPress={onDecline}>No</Button>
+        </CardSection>
+      </View>
     </Modal>
   );
 };
+
+const styles = StyleSheet.create({
+  cardSectionStyle: {
+    justifyContent: 'center',
+  },
+  textStyle: {
+    flex: 1,
+    fontSize: 18,
+    lineHeight: 40,
+  },
+  containerStyle: {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    position: 'relative',
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
 
 export { Confirm };
