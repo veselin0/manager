c0319f0eff242068b6f5ef666f94a91ab34af7e8
Veselin <vesselin60@gmail.com>
2020-12-30 09:31:58 +0200

    423. Employee Delete Action Creator

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -32,6 +32,14 @@ class EmployeeEdit extends Component {
     Communications.text(phone, `Your-upcoming-shift-is-on-${shift}`);
   }
 
+  onAccept() {
+
+  }
+
+  onDecline() {
+    this.setState({ showModal: false });
+  }
+
   render() {
     return (
       <Card>
@@ -52,7 +60,10 @@ class EmployeeEdit extends Component {
           </Button>
         </CardSection>
 
-        <Confirm visible={this.state.showModal}>
+        <Confirm
+          visible={this.state.showModal}
+          onAccept={this.onAccept.bind(this)}
+          onDecline={this.onDecline.bind(this)}>
           Are you sure you want to delete this?
         </Confirm>
       </Card>
